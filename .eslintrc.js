7b518d5924e78e5fda9459b2124ac5923a7a5269
marcobcchen <marco.chen@hngf.com.tw>
2020-10-21 15:55:07 +0800

    Add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,10 @@ module.exports = {
     'react',
   ],
   rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }], // 取消 jsx 格式限制
+    'import/no-unresolved': [2, { ignore: ["^@/"] }], // 不檢查自訂 import @ 的路徑
     'import/no-extraneous-dependencies': [0], // 不提醒非 dev 的依賴包
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }], // 取消 jsx 格式限制
+    'react/jsx-one-expression-per-line': [0], // jsx中的表達式不換行
+    'react/prop-types': [0], // 不進行 prop-types 驗證
   },
 };
